7863058243067682fd864f732e28e22b366b086f
Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>
2026-02-24 22:59:48 +0530

    fix 404 2
    Signed-off-by: Rajesh-Nagarajan-11 <rajeshnagarajan36@gmail.com>

--- static/scripts/kanvas-corner-popup.js
+++ static/scripts/kanvas-corner-popup.js
@@ -1,16 +1,13 @@
 (function () {
   "use strict";
 
-  var IMG_BASE = "/images/banner-transitions/";
   var CLOSE_SVG =
     '<svg width="28" height="28" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">' +
     '<path fill-rule="evenodd" clip-rule="evenodd" d="M12 2C6.47 2 2 6.47 2 12C2 17.53 6.47 22 12 22C17.53 22 22 17.53 22 12C22 6.47 17.53 2 12 2ZM17 15.59L15.59 17L12 13.41L8.41 17L7 15.59L10.59 12L7 8.41L8.41 7L12 10.59L15.59 7L17 8.41L13.41 12L17 15.59Z"/>' +
     "</svg>";
 
-  var CANVAS_SRC = IMG_BASE + "empty-dark.svg";
-
   function img(cls, file) {
-    return '<img class="' + cls + '" src="' + IMG_BASE + file + '" alt="" />';
+    return '<img class="' + cls + '" src="/images/banner-transitions/' + file + '" alt="" />';
   }
 
   function buildPopup() {
@@ -26,12 +23,12 @@
       '<div class="kanvas-transition-container">' +
       '<div class="canvas-grid"></div>' +
       '<img class="kanvas-canvas-bg" src="/images/banner-transitions/empty-dark.svg" alt="" />' +
-      '<div class="kanvas-layer"><img class="kanvas-service-interface" src="/images/banner-transitions/service-interface-partial-colorMode.svg" alt="" /></div>' +
-      '<div class="kanvas-layer"><img class="kanvas-ingress-gateway" src="/images/banner-transitions/ingress-gateway-partial-colorMode.svg" alt="" /></div>' +
-      '<div class="kanvas-layer"><img class="kanvas-kubernetes" src="/images/banner-transitions/kubernetes-partial-colorMode.svg" alt="" /></div>' +
-      '<div class="kanvas-layer"><img class="kanvas-pod" src="/images/banner-transitions/pod-partial-colorMode.svg" alt="" /></div>' +
-      '<div class="kanvas-layer"><img class="kanvas-prometheus" src="/images/banner-transitions/prometheus-partial-colorMode.svg" alt="" /></div>' +
-      '<img class="kanvas-supporting-arrows" src="/images/banner-transitions/supporting-arrows.svg" alt="" />' +
+      '<div class="kanvas-layer">' + img("kanvas-service-interface", "service-interface-partial-colorMode.svg") + "</div>" +
+      '<div class="kanvas-layer">' + img("kanvas-ingress-gateway", "ingress-gateway-partial-colorMode.svg") + "</div>" +
+      '<div class="kanvas-layer">' + img("kanvas-kubernetes", "kubernetes-partial-colorMode.svg") + "</div>" +
+      '<div class="kanvas-layer">' + img("kanvas-pod", "pod-partial-colorMode.svg") + "</div>" +
+      '<div class="kanvas-layer">' + img("kanvas-prometheus", "prometheus-partial-colorMode.svg") + "</div>" +
+      img("kanvas-supporting-arrows", "supporting-arrows.svg") +
       "</div>" +
       '<div class="try-it-text"><span>Try it now at </span><strong>kanvas.new</strong></div>' +
       "</a>" +
